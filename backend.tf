1251ebbec62d96ae8340503d1f4218e7efd76871
mediDEU <mehdivasef@gmail.com>
2025-06-03 15:31:22 +0200

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "githubpoc"
+    storage_account_name = "stcstatefiletfpoc"
+    container_name       = "tfstate"
+    key                  = "tf.tfstate"
+    tenant_id            = "023a7871-db5b-45bd-b776-88e3cd7e0b2c"
+    subscription_id      = "b307668a-47b5-4933-b51f-316827f474fa"
+    }
+}
